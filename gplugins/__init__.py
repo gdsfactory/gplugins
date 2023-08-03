64d91ce16c69ad1f3b595915b43540c69482b87a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-03 16:24:03 -0700

    Bump version: 0.0.2 → 0.0.3

--- gplugins/__init__.py
+++ gplugins/__init__.py
@@ -1,6 +1,6 @@
 """gplugins - gdsfactory plugins"""
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from gplugins.utils import plot, port_symmetries
 from gplugins.utils.get_effective_indices import get_effective_indices
