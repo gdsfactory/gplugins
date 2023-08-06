60f8de3d19d0a7f224dcba7c6bb355c5ecbd3457
simbilod <sb30@princeton.edu>
2023-08-06 12:51:13 -0400

    draft materials plugin

---
+++ gplugins/materials/semiconductor/semiconductor_mat.py
@@ -0,0 +1,10 @@
+class SemiconductorMaterial:
+    def __init__(self, ureg=None):
+        """Semiconductor parameters."""
+        self.ureg = ureg
+        self.m_e = None
+        self.m_hh = None
+        self.m_lh = None
+        self.eps = None
+        self.Eg = None
+        self.ni = None
