3021f5bffac259b8829901dc66115ddeaf69ac1f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-09-19 06:47:59 -0700

    remove deprecated modules and pin sax

--- gplugins/fem/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import warnings
-
-from gplugins import femwell
-
-message = "gplugins.fem has been renamed to gplugins.femwell. Please update your code."
-warnings.warn(message)
-sys.modules["gplugins.fem"] = femwell
