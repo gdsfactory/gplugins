401bcf5d38269ec37078dfcb0035c758e1a65fee
Yannick Augenstein <yaugenst@google.com>
2023-11-16 11:03:08 -0800

    Add docstrings

--- gplugins/tidy3d/component.py
+++ gplugins/tidy3d/component.py
@@ -1,3 +1,19 @@
+"""
+This module contains the Tidy3DComponent class which is used to model 3D components in the Tidy3D simulation environment.
+It extends the LayeredComponentBase class and adds additional properties and methods specific to the Tidy3D environment.
+
+Classes:
+    Tidy3DComponent: Represents a 3D component in the Tidy3D simulation environment.
+
+Functions:
+    polyslabs: Returns a dictionary of PolySlab instances for each layer in the component.
+    structures: Returns a list of Structure instances for each PolySlab in the component.
+    get_ports: Returns a list of Port instances for each optical port in the component.
+    get_simulation: Returns a Simulation instance for the component.
+    get_component_modeler: Returns a ComponentModeler instance for the component.
+    plot_slice: Plots a cross section of the component at a specified position.
+"""
+
 from functools import cached_property
 
 import matplotlib.pyplot as plt
@@ -13,6 +29,19 @@ from .util import get_port_normal, sort_layers
 
 
 class Tidy3DComponent(LayeredComponentBase):
+    """
+    Represents a 3D component in the Tidy3D simulation environment.
+
+    Attributes:
+        material_mapping (dict[str, Tidy3DMedium]): A mapping of material names to Tidy3DMedium instances.
+        extend_ports (NonNegativeFloat): The extension length for ports.
+        port_offset (float): The offset for ports.
+        pad_xy_inner (NonNegativeFloat): The inner padding in the xy-plane.
+        pad_xy_outer (NonNegativeFloat): The outer padding in the xy-plane.
+        pad_z_inner (float): The inner padding in the z-direction.
+        pad_z_outer (NonNegativeFloat): The outer padding in the z-direction.
+    """
+
     material_mapping: dict[str, Tidy3DMedium]
     extend_ports: NonNegativeFloat = 1.0
     port_offset: float = 0.2
@@ -23,6 +52,12 @@ class Tidy3DComponent(LayeredComponentBase):
 
     @cached_property
     def polyslabs(self) -> dict[str, tuple[td.PolySlab, ...]]:
+        """
+        Returns a dictionary of PolySlab instances for each layer in the component.
+
+        Returns:
+            dict[str, tuple[td.PolySlab, ...]]: A dictionary mapping layer names to tuples of PolySlab instances.
+        """
         slabs = {}
         layers = sort_layers(self.geometry_layers, sort_by="mesh_order", reverse=True)
         for name, layer in layers.items():
@@ -41,6 +76,12 @@ class Tidy3DComponent(LayeredComponentBase):
 
     @cached_property
     def structures(self) -> list[td.Structure]:
+        """
+        Returns a list of Structure instances for each PolySlab in the component.
+
+        Returns:
+            list[td.Structure]: A list of Structure instances.
+        """
         structures = []
         for name, polys in self.polyslabs.items():
             structures.extend(
@@ -63,6 +104,17 @@ class Tidy3DComponent(LayeredComponentBase):
         size_mult: float | tuple[float, float] = (4.0, 2.0),
         cz: float | None = None,
     ) -> list[Port]:
+        """
+        Returns a list of Port instances for each optical port in the component.
+
+        Args:
+            mode_spec (td.ModeSpec): The mode specification for the ports.
+            size_mult (float | tuple[float, float], optional): The size multiplier for the ports. Defaults to (4.0, 2.0).
+            cz (float | None, optional): The z-coordinate for the ports. If None, the z-coordinate of the component is used. Defaults to None.
+
+        Returns:
+            list[Port]: A list of Port instances.
+        """
         ports = []
         for port in self.ports:
             if port.port_type != "optical":
@@ -99,6 +151,20 @@ class Tidy3DComponent(LayeredComponentBase):
         run_time: float = 1e-12,
         shutoff: float = 1e-5,
     ) -> td.Simulation:
+        """
+        Returns a Simulation instance for the component.
+
+        Args:
+            grid_spec (td.GridSpec): The grid specification for the simulation.
+            center_z (float): The z-coordinate for the center of the simulation.
+            sim_size_z (int): The size of the simulation in the z-direction.
+            boundary_spec (td.BoundarySpec): The boundary specification for the simulation.
+            run_time (float, optional): The run time for the simulation. Defaults to 1e-12.
+            shutoff (float, optional): The shutoff value for the simulation. Defaults to 1e-5.
+
+        Returns:
+            td.Simulation: A Simulation instance.
+        """
         sim_center = (*self.center[:2], center_z)
         sim_size = (*self.size[:2], sim_size_z)
         sim = td.Simulation(
@@ -131,6 +197,30 @@ class Tidy3DComponent(LayeredComponentBase):
         path_dir: str = ".",
         verbose: bool = True,
     ) -> ComponentModeler:
+        """
+        Returns a ComponentModeler instance for the component.
+
+        Args:
+            wavelength (float, optional): The wavelength for the ComponentModeler. Defaults to 1.55.
+            bandwidth (float, optional): The bandwidth for the ComponentModeler. Defaults to 0.2.
+            num_freqs (int, optional): The number of frequencies for the ComponentModeler. Defaults to 21.
+            min_steps_per_wvl (int, optional): The minimum number of steps per wavelength for the ComponentModeler. Defaults to 30.
+            center_z (float | str | None, optional): The z-coordinate for the center of the ComponentModeler. If None, the z-coordinate of the component is used. Defaults to None.
+            sim_size_z (int, optional): The size of the simulation in the z-direction for the ComponentModeler. Defaults to 4.
+            port_size_mult (float | tuple[float, float], optional): The size multiplier for the ports in the ComponentModeler. Defaults to (4.0, 3.0).
+            run_only (tuple[tuple[str, int], ...] | None, optional): The run only specification for the ComponentModeler. Defaults to None.
+            element_mappings (Tidy3DElementMapping, optional): The element mappings for the ComponentModeler. Defaults to ().
+            mode_spec (td.ModeSpec, optional): The mode specification for the ComponentModeler. Defaults to td.ModeSpec(num_modes=1, filter_pol="te").
+            boundary_spec (td.BoundarySpec, optional): The boundary specification for the ComponentModeler. Defaults to td.BoundarySpec.all_sides(boundary=td.PML()).
+            run_time (float, optional): The run time for the ComponentModeler. Defaults to 1e-12.
+            shutoff (float, optional): The shutoff value for the ComponentModeler. Defaults to 1e-5.
+            folder_name (str, optional): The folder name for the ComponentModeler. Defaults to "default".
+            path_dir (str, optional): The directory path for the ComponentModeler. Defaults to ".".
+            verbose (bool, optional): Whether to print verbose output for the ComponentModeler. Defaults to True.
+
+        Returns:
+            ComponentModeler: A ComponentModeler instance.
+        """
         match center_z:
             case float():
                 cz = center_z
@@ -183,6 +273,20 @@ class Tidy3DComponent(LayeredComponentBase):
         ax: plt.Axes | None = None,
         legend: bool = False,
     ) -> plt.Axes:
+        """
+        Plots a cross section of the component at a specified position.
+
+        Args:
+            x (float | str | None, optional): The x-coordinate for the cross section. If None, the x-coordinate of the component is used. Defaults to None.
+            y (float | str | None, optional): The y-coordinate for the cross section. If None, the y-coordinate of the component is used. Defaults to None.
+            z (float | str | None, optional): The z-coordinate for the cross section. If None, the z-coordinate of the component is used. Defaults to None.
+            offset (float, optional): The offset for the cross section. Defaults to 0.0.
+            ax (plt.Axes | None, optional): The Axes instance to plot on. If None, a new Axes instance is created. Defaults to None.
+            legend (bool, optional): Whether to include a legend in the plot. Defaults to False.
+
+        Returns:
+            plt.Axes: The Axes instance with the plot.
+        """
         x, y, z = (
             self.get_layer_center(c)[i] if isinstance(c, str) else c
             for i, c in enumerate((x, y, z))
--- gplugins/tidy3d/util.py
+++ gplugins/tidy3d/util.py
@@ -7,12 +7,35 @@ from gdsfactory.technology.layer_stack import LayerLevel
 def sort_layers(
     layers: dict[str, LayerLevel], sort_by: str, reverse: bool = False
 ) -> dict[str, LayerLevel]:
+    """
+    Sorts a dictionary of LayerLevel objects based on a specified attribute.
+
+    Args:
+        layers (dict[str, LayerLevel]): A dictionary where the keys are layer names and the values are LayerLevel objects.
+        sort_by (str): The attribute of the LayerLevel objects to sort by. This can be 'zmin', 'zmax', 'zcenter', or 'thickness'.
+        reverse (bool, optional): If True, the layers are sorted in descending order. Defaults to False.
+
+    Returns:
+        dict[str, LayerLevel]: A dictionary of LayerLevel objects, sorted by the specified attribute.
+    """
     return dict(
         sorted(layers.items(), key=lambda x: getattr(x[1], sort_by), reverse=reverse)
     )
 
 
 def get_port_normal(port: Port) -> tuple[int, Literal["+", "-"]]:
+    """
+    This function returns the index of the normal axis (x,y,z) and the tidy3d port orientation string.
+
+    Args:
+        port (Port): A gdsfactory Port object.
+
+    Returns:
+        tuple: A tuple containing the index of the normal axis and the tidy3d port orientation string.
+
+    Raises:
+        ValueError: If the orientation does not match any of the standard orientations.
+    """
     match ort := port.orientation:
         case 0:
             return 0, "-"
