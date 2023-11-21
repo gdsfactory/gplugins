733241c49744c7ca210a9d49f9c66c0956cb6ac8
Niko Savola <nikosavola@google.com>
2023-11-21 14:28:43 -0800

    Accept more widely-used SPICE file extensions in `plot_nets`

--- gplugins/klayout/plot_nets.py
+++ gplugins/klayout/plot_nets.py
@@ -103,7 +103,7 @@ def plot_nets(
             l2n = kdb.LayoutToNetlist()
             l2n.read(str(filepath))
             netlist = l2n.netlist()
-        case ".spi" | ".spice":
+        case ".cir" | ".sp" | ".spi" | ".spice":
             reader = kdb.NetlistSpiceReader(NoCommentReader())
             netlist = kdb.Netlist()
             netlist.read(str(filepath), reader)
