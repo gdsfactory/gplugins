6d879a1c0ff884b097d24ecab2c6cedcf7a72903
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-11-21 07:03:47 -0800

    improve tests

--- gplugins/tidy3d/tests/test_write_sparameters.py
+++ gplugins/tidy3d/tests/test_write_sparameters.py
@@ -5,13 +5,13 @@ import gdsfactory as gf
 import gplugins.tidy3d as gt
 
 
-def test_plot_sparameters_3d() -> None:
+def test_write_sparameters_3d() -> None:
     """Checks simulation for a straight waveguide in 2D."""
     c = gf.components.straight(length=2)
     gt.write_sparameters(c, run=False, sim_size_z=4)
 
 
-def test_plot_sparameters_2d() -> None:
+def test_write_sparameters_2d() -> None:
     """Checks simulation for a straight waveguide in 2D."""
     c = gf.components.straight(length=2)
     gt.write_sparameters(c, run=False, sim_size_z=0)
