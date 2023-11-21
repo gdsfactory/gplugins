1464ae0523b1b8b8088ccfe77890ebb7020c04c0
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-11-20 18:19:39 -0800

    fix tests

--- gplugins/tidy3d/tests/test_plot_simulation.py
+++ gplugins/tidy3d/tests/test_plot_simulation.py
@@ -8,20 +8,10 @@ import gplugins.tidy3d as gt
 def test_plot_sparameters_3d() -> None:
     """Checks simulation for a straight waveguide in 2D."""
     c = gf.components.straight(length=2)
-    gt.write_sparameters_1x1(c, is_3d=True, run=False)
+    gt.write_sparameters(c, run=False, sim_size_z=4)
 
 
 def test_plot_sparameters_2d() -> None:
     """Checks simulation for a straight waveguide in 2D."""
     c = gf.components.straight(length=2)
-    gt.write_sparameters_1x1(c, is_3d=False, run=False)
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    c = gf.components.straight(length=2)
-    is_3d = True
-    is_3d = False
-    gt.write_sparameters_1x1(c, run=False, is_3d=is_3d)
-    plt.show()
+    gt.write_sparameters(c, run=False, sim_size_z=0)
