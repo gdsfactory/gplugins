14701fb25c2b545b231a91d10836064f6d951f09
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-12-15 17:03:32 +0900

    sort function

--- gplugins/klayout/drc/count_drc.py
+++ gplugins/klayout/drc/count_drc.py
@@ -7,10 +7,14 @@ from klayout import rdb
 PathType = pathlib.Path | str
 
 
-def count_drc(rdb_path: PathType) -> dict[str, int]:
+def count_drc(rdb_path: PathType, threshold: int = 0) -> dict[str, int]:
     """Reads klayout Results database and returns a dict of error names to number of errors.
 
     Works for both single rdb files and directories of rdb files.
+
+    Args:
+        rdb_path: Path to rdb file or directory of rdb files.
+        threshold: Minimum number of errors to be included in the output.
     """
     rdb_path = pathlib.Path(rdb_path)
     errors_dict = {}
@@ -32,13 +36,24 @@ def count_drc(rdb_path: PathType) -> dict[str, int]:
             errors_per_category = r.each_item_per_category(category_id)
             errors = len(list(errors_per_category))
 
-            if errors > 0:
+            if errors > threshold:
                 errors_dict[category.name()] = errors
 
-    return errors_dict
+    return dict(sorted(errors_dict.items(), key=lambda item: item[1], reverse=True))
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     rdb_path = pathlib.Path("/home/jmatres/Downloads/demo.rdb")
-    errors = count_drc(rdb_path)
+    errors = count_drc(rdb_path, threshold=100)
+
+    plt.bar(errors.keys(), errors.values())
+
+    # Adding titles and labels
+    plt.title("Histogram of Data")
+    plt.xlabel("Categories")
+    plt.ylabel("Errors")
+    plt.show()
+
     # error_count = count_drc(rdb_path)
