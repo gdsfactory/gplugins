930ef641edfd8c71fee9dd5f17445248472a227d
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-12-31 08:47:38 +0900

    simpler meep install

--- gplugins/gmeep/test_materials.py
+++ gplugins/gmeep/test_materials.py
@@ -71,5 +71,5 @@ def test_materials_override_complex() -> None:
 
 
 if __name__ == "__main__":
-    # test_materials_override()
-    test_materials_override_complex()
+    test_materials_override()
+    # test_materials_override_complex()
