44db1e6b20a7cb8aa3b385cc985f0f31ca00dd8c
flaport <floris.laporte@gmail.com>
2024-03-23 13:48:42 -0700

    ignore warnings

--- gplugins/gfviz/gfviz.py
+++ gplugins/gfviz/gfviz.py
@@ -1,5 +1,6 @@
 import json
 import os
+import warnings
 from copy import deepcopy
 
 import gdsfactory as gf
@@ -36,27 +37,29 @@ def load_sample_netlist():
 
 
 def schemedit_html(comp_or_net, height=None, patch=True):
-    if isinstance(comp_or_net, str):
-        comp_or_net = gf.get_component(comp_or_net)
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
+        if isinstance(comp_or_net, str):
+            comp_or_net = gf.get_component(comp_or_net)
 
-    if isinstance(comp_or_net, gf.Component):
-        netlist = get_netlist(comp_or_net)
-    else:
-        netlist = deepcopy(comp_or_net)
-    if patch:
-        netlist = patch_netlist(netlist)
-    if not height:
-        height = 0
-    height = int(height)
-    template = jinja.get_template("index.html")
-    schemedit_js = open(os.path.join(STATIC_DIR, "schemedit.js")).read()
-    schemedit_b64 = load_schemedit_wasm_b64()
-    return template.render(
-        netlist=json.dumps(netlist),
-        schemedit_js=schemedit_js,
-        schemedit_b64=schemedit_b64,
-        height=height,
-    )
+        if isinstance(comp_or_net, gf.Component):
+            netlist = get_netlist(comp_or_net)
+        else:
+            netlist = deepcopy(comp_or_net)
+        if patch:
+            netlist = patch_netlist(netlist)
+        if not height:
+            height = 0
+        height = int(height)
+        template = jinja.get_template("index.html")
+        schemedit_js = open(os.path.join(STATIC_DIR, "schemedit.js")).read()
+        schemedit_b64 = load_schemedit_wasm_b64()
+        return template.render(
+            netlist=json.dumps(netlist),
+            schemedit_js=schemedit_js,
+            schemedit_b64=schemedit_b64,
+            height=height,
+        )
 
 
 def show(netlist, height=300):
