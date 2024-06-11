0a8647750927b197190e4045f15c7f5c59198e4c
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-11 17:21:17 +0200

    fix more tests

--- gplugins/lumerical/write_sparameters_lumerical.py
+++ gplugins/lumerical/write_sparameters_lumerical.py
@@ -9,7 +9,8 @@ from typing import TYPE_CHECKING
 import gdsfactory as gf
 import numpy as np
 import yaml
-from gdsfactory.config import __version__, logger
+from gdsfactory import logger
+from gdsfactory.config import __version__
 from gdsfactory.generic_tech.simulation_settings import (
     SIMULATION_SETTINGS_LUMERICAL_FDTD,
     SimulationSettingsLumericalFdtd,
