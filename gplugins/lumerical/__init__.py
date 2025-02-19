ad45625b1000b0a93ff9fc5be358b5afb176eaf1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-02-18 16:25:48 -0800

    reomve dagster plugin

--- gplugins/lumerical/__init__.py
+++ gplugins/lumerical/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from gplugins.lumerical.interconnect import run_wavelength_sweep
 from gplugins.lumerical.read import read_sparameters_lumerical
 from gplugins.lumerical.write_sparameters_lumerical import (
     write_sparameters_lumerical,
@@ -11,7 +10,6 @@ from gplugins.lumerical.write_sparameters_lumerical_components import (
 
 __all__ = [
     "read_sparameters_lumerical",
-    "run_wavelength_sweep",
     "write_sparameters_lumerical",
     "write_sparameters_lumerical_components",
 ]
